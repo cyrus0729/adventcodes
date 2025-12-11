52d02a38ef439aebb02ae794be8b2909487a8d45
aletris <104312459+cyrus0729@users.noreply.github.com>
2025-12-11 14:02:29 +0800

    Create 11p1.py

---
+++ cadvent2025/11p1.py
@@ -0,0 +1,13 @@
+txt = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out""" # replace with read later
+
+data = txt.split("\n").split(" ")
+print(data)
